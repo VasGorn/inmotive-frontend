3c43e08cb0aa97b5e185a9d9ac3da35b57e7f268
VasGorn <vas.goroshko@gmail.com>
2022-02-22 12:20:07 +0200

    feat: create container of header components

---
+++ src/app/shared/header/containers/header/header.component.ts
@@ -0,0 +1,98 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { Router } from "@angular/router";
+
+import { AuthService } from "../../../../pages/auth/services";
+import { routes } from "../../../../consts";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Observable, Subscription, throwError } from "rxjs";
+import { NotificationService } from "src/app/pages/notification/notification.service";
+import { ProjectService } from "src/app/pages/projects/services/project.service";
+import { Project } from "src/app/pages/projects/models/project";
+import { map, take } from "rxjs/operators";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Input() isMenuOpened: boolean;
+  @Output() isShowSidebar = new EventEmitter<boolean>();
+  public routers: typeof routes = routes;
+  public clicked: boolean = false;
+  public firstFiveProjects: Project[] = [];
+  public selectedProject: Project;
+
+  private projectsSub: Subscription;
+  private projectSub: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private notification: NotificationService,
+    private router: Router,
+    private projectService: ProjectService
+  ) {}
+
+  ngOnInit(): void {
+    this.projectsSub = this.projectService.projectsSubject$.subscribe(
+      (projects: Project[]) => {
+        let projectsCopy = [...projects];
+        this.firstFiveProjects =
+          this.sortByDateAndReturnFiveElements(projectsCopy);
+      }
+    );
+    this.projectSub = this.projectService.projectSubject$.subscribe(
+      (project: Project) => {
+        this.selectedProject = project;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.projectsSub.unsubscribe();
+    this.projectSub.unsubscribe();
+  }
+
+  public selectProject($event: Project) {
+    this.projectService.projectSubject$.next($event);
+  }
+
+  public openMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+    this.isShowSidebar.emit(this.isMenuOpened);
+  }
+
+  public logout(): void {
+    this.authService.logout().subscribe({
+      next: (data: string) => {
+        this.notification.showInfoToastr(data);
+        console.log(data);
+        this.router.navigateByUrl(this.routers.LOGIN);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.notification.showErrorToastr(error.message);
+        throwError(() => console.log(error.message));
+      },
+    });
+  }
+
+  private sortByDateAndReturnFiveElements(projects: Project[]): Project[] {
+    if (projects.length < 5) {
+      return projects.sort((a: Project, b: Project) => {
+        return a.created < b.created ? 1 : -1;
+      });
+    }
+    return projects
+      .sort((a: Project, b: Project) => {
+        return a.created < b.created ? 1 : -1;
+      })
+      .splice(0, 5);
+  }
+}
