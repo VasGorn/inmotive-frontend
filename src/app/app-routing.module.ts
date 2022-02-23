91ee02b08ec505c47749112bb790c3bb05cd90fd
VasGorn <vas.goroshko@gmail.com>
2022-02-23 09:43:30 +0200

    feat: add to app routes path to the login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./pages/auth/auth.module").then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [
