f6744a1fcd6079f46181cb4027296604496c2bed
VasGorn <vas.goroshko@gmail.com>
2022-02-25 09:16:21 +0200

    feat: add to app routes path to the project page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { AuthGuard } from "./pages/auth/guards/auth.guard";
+import { ProjectPageComponent } from "./pages/projects/containers/project-page/project-page.component";
 
 const routes: Routes = [
   {
     path: "login",
     loadChildren: () =>
       import("./pages/auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "projects",
+    pathMatch: "full",
+    canActivate: [AuthGuard],
+    component: ProjectPageComponent,
+    loadChildren: () =>
+      import("./pages/projects/projects.module").then((m) => m.ProjectsModule),
   },
 ];
 
