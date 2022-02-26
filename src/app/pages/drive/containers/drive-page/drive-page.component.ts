1aa740b9ae9a64294de06352cb71de510978c9f5
VasGorn <vas.goroshko@gmail.com>
2022-02-26 09:10:03 +0200

    feat: create container of driver page

---
+++ src/app/pages/drive/containers/drive-page/drive-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-drive-page",
+  templateUrl: "./drive-page.component.html",
+  styleUrls: ["./drive-page.component.scss"],
+})
+export class DrivePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
